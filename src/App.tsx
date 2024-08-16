4ee992d85c7033e25ec1d6d22c41fff6b041ef52
H3nryK <henrykimani89@gmail.com>
2024-08-16 11:26:11 +0300

    Shop page import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ShopPage from './pages/Shoppage';
+import ShopPage from './pages/ShopPage';
 import ProductDetailsPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
